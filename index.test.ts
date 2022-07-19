2bcf90d0c6a3ca09eab220ca183c44e92fe1af1f
LAWTED <3209571656@qq.com>
2022-07-19 14:50:25 +0800

    fix: lint and format

--- index.test.ts
+++ index.test.ts
@@ -6,7 +6,7 @@ const fixtureFirefoxStack = `
   calc@http://192.168.31.8:8000/a.js:4:3
   <anonymous>:1:11
   http://192.168.31.8:8000/a.js:22:3
-`
+`;
 
 const fixtureStack = `TypeError: Error raised
   at bar http://192.168.31.8:8000/c.js:2:9
@@ -14,31 +14,36 @@ const fixtureStack = `TypeError: Error raised
   at calc http://192.168.31.8:8000/a.js:4:3
   at <anonymous>:1:11
   at http://192.168.31.8:8000/a.js:22:3
-`
+`;
 const correct = [
-  { line: 2, column: 9, filename: 'http://192.168.31.8:8000/c.js' },
-  { line: 4, column: 15, filename: 'http://192.168.31.8:8000/b.js' },
-  { line: 4, column: 3, filename: 'http://192.168.31.8:8000/a.js' },
-  { line: 22, column: 3, filename: 'http://192.168.31.8:8000/a.js' },
-]
+  { line: 2, column: 9, filename: "http://192.168.31.8:8000/c.js" },
+  { line: 4, column: 15, filename: "http://192.168.31.8:8000/b.js" },
+  { line: 4, column: 3, filename: "http://192.168.31.8:8000/a.js" },
+  { line: 22, column: 3, filename: "http://192.168.31.8:8000/a.js" },
+];
 
 function initError(error: string): Error {
   return {
     stack: error,
-    message: '',
-    name: 'Error'
-  }
+    message: "",
+    name: "Error",
+  };
 }
 
-describe('parseError', () => {
-  test('empty', () => {
-    expect(parseError(initError(''))).toEqual({ message: '', stack: [] })
-  })
-  test('firefox', () => {
-    expect(parseError(initError(fixtureFirefoxStack))).toEqual({ message: '', stack: correct })
-  })
-  test('chrome', () => {
-    expect(parseError(initError(fixtureStack))).toEqual({ message: 'Error raised', stack: correct })
-  })
-})
-
+describe("parseError", () => {
+  test("empty", () => {
+    expect(parseError(initError(""))).toEqual({ message: "", stack: [] });
+  });
+  test("firefox", () => {
+    expect(parseError(initError(fixtureFirefoxStack))).toEqual({
+      message: "",
+      stack: correct,
+    });
+  });
+  test("chrome", () => {
+    expect(parseError(initError(fixtureStack))).toEqual({
+      message: "Error raised",
+      stack: correct,
+    });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,32 @@
 export interface ErrorMessage {
-  message: string
+  message: string;
   stack: Array<{
-    line: number
-    column: number
-    filename: string
-  }>
+    line: number;
+    column: number;
+    filename: string;
+  }>;
 }
 
 export function parseError(err: Error): ErrorMessage {
-  const errArr = (err.stack || '').split('\n')
+  const errArr = (err.stack || "").split("\n");
   if (errArr.length < 2) {
-    return {message: '', stack: []}
+    return { message: "", stack: [] };
   }
-  const message = errArr[0].split(':')[1]?.trim() || ''
-  console.log(errArr)
-  const stack: Array<{line: number, column: number, filename: string}> = errArr.slice(1).filter(line => line.includes('http')).map(line => {
-    const [,filename, lineStr, columnStr] = line.match(/^.*(http?:\/\/.+):(\d+):(\d+)/) || []
-    return {
-      line: parseInt(lineStr),
-      column: parseInt(columnStr),
-      filename: filename
-    }
-  })
+  const message = errArr[0].split(":")[1]?.trim() || "";
+  console.log(errArr);
+  const stack: Array<{ line: number; column: number; filename: string }> =
+    errArr
+      .slice(1)
+      .filter((line) => line.includes("http"))
+      .map((line) => {
+        const [, filename, lineStr, columnStr] =
+          line.match(/^.*(http?:\/\/.+):(\d+):(\d+)/) || [];
+        return {
+          line: parseInt(lineStr),
+          column: parseInt(columnStr),
+          filename: filename,
+        };
+      });
 
-  return { message, stack }
+  return { message, stack };
 }
